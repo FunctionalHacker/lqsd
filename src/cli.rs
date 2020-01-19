8d25715e55b0cdfea0f2b117fe6a22425884bc1f
Marko Korhonen <marko.korhonen@reekynet.com>
2020-01-19 23:16:32 +0200

    Require argument, else display help
    Signed-off-by: Marko Korhonen <marko.korhonen@reekynet.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,12 @@
 extern crate clap;
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg};
 
 pub fn get_args() -> clap::ArgMatches<'static> {
     App::new("LiQuid Screen Dim")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::ArgRequiredElseHelp)
         .arg(
             Arg::with_name("dim")
                 .long("dim")
